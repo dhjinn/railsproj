744ff84b162839f297e81157c0e073eccec96015
Derek Jin <derekjin@berkeley.edu>
2018-04-19 19:56:41 -0700

    models and controllers and gems

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,4 +1,5 @@
 class Club < ApplicationRecord
 	has_many :users
+	has_one :admin
 	has_many :transactions, through: :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420020425) do
+ActiveRecord::Schema.define(version: 20180420025330) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "clubs", force: :cascade do |t|
     t.integer "budget"
